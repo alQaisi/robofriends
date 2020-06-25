f125f9cbe2df202df0538dfa99f4f8e169708967
ahmedhamed1312@gmail.com <ahmedhamed1312@gmail.com>
2020-06-25 17:27:35 +0300

    Styling Images

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,8 +1,9 @@
 import React from 'react';
+import './Card.css';
 const Card=({name,email,id,click})=>{
     return(
        <div onClick={click.bind(this,name)} className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
-           <img alt="robot" src={`https://robohash.org/${id}?200x200`} />
+           <img className="img-card" alt="robot" src={`https://robohash.org/${id}?200x200`} />
            <div>
                <h2>{name}</h2>
                <p>{email}</p>
